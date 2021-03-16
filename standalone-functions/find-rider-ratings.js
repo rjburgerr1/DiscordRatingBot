8613f8e36fde230c12c5d7421d9121dcb23d7225
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-03-16 01:57:09 -0500

    Add filter for level ranges, as well as error message for null results

--- standalone-functions/find-rider-ratings.js
+++ standalone-functions/find-rider-ratings.js
@@ -1,12 +1,33 @@
 const { tracksDB } = require("../data/mongodb-utility");
-const getRider = async (rider) => {
-  const riderDocs = tracksDB
-    .collection("ratings")
-    .find({ author: rider })
-    .collation({ locale: "en", strength: 1 })
-    .sort({ level_opinion: -1 });
+const getRider = async (rider, levelFilter) => {
+  try {
+    let riderDocs;
+    if (levelFilter === undefined) {
+      riderDocs = tracksDB
+        .collection("ratings")
+        .find({
+          author: rider,
+        })
+        .collation({ locale: "en", strength: 1 })
+        .sort({ level_opinion: -1 });
+    } else {
+      riderDocs = tracksDB
+        .collection("ratings")
+        .find({
+          author: rider,
+          level_opinion: {
+            $gte: Number(levelFilter),
+            $lt: Number(levelFilter) + 1,
+          },
+        })
+        .collation({ locale: "en", strength: 1 })
+        .sort({ level_opinion: -1 });
+    }
 
-  return riderDocs.toArray();
+    return riderDocs.toArray();
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 module.exports.getRider = getRider;
