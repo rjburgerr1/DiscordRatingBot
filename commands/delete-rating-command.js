028fc71175e79d5e6920beadedf618f3c70dcf4b
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-03-20 23:43:12 -0500

    Add extra arguments to account for change of basic collector addition

--- commands/delete-rating-command.js
+++ commands/delete-rating-command.js
@@ -1,22 +1,33 @@
 const {
   deleteDocument,
 } = require("../standalone-functions/delete-rider-rating");
-const collectBasic = require("../standalone-functions/message-collector");
+const { collectBasic } = require("../standalone-functions/message-collector");
 
 module.exports = {
   name: "delete",
   description: "Deletes a track rating of your own",
   async execute(message, args) {
-    const track = await getTrackArgument(message, args, "delete");
+    const track = await getTrackArgument(message, args);
 
     deleteDocument(message.author, track);
   },
 };
 
-const getTrackArgument = async (message, args, rateStr) => {
+const getTrackArgument = async (message, args) => {
+  const trackArgumentFilter = (msg) => {
+    if (!msg.author.bot) {
+      return true;
+    }
+  };
   let track;
   if (args[0] === undefined) {
-    track = await collectBasic(message.author, message, rateStr);
+    track = await collectBasic(
+      message.author,
+      message,
+      "```Type the name of the track that you want to delete.```",
+      20000,
+      trackArgumentFilter
+    );
   } else {
     // Track Name
     track = args[0];
