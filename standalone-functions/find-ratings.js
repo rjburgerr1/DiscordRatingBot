b942ad5d4da6467b91489b5bb86171629346cbd6
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-03-21 01:55:59 -0500

    Programmatically add query filters dependent on argument inclusions

--- standalone-functions/find-ratings.js
+++ standalone-functions/find-ratings.js
@@ -1,48 +1,36 @@
 const { tracksDB } = require("../data/mongodb-utility.js");
-const findRatings = async (trackName) => {
-  // DRY (maybe( cannot apply here as server-side functionality mixed with mongodb pipelines makes using external variables for the track filter very hard to inject.
-  if (trackName === undefined) {
-    return await tracksDB
-      .collection("ratings")
-      .aggregate([
-        {
-          $group: {
-            _id: "$track",
-            level_average: { $avg: "$level_opinion" },
-            count: { $sum: 1 },
-            lowestRating: {
-              $min: { minRating: "$level_opinion", author: "$author" },
-            },
-            highestRating: {
-              $max: { maxRating: "$level_opinion", author: "$author" },
-            },
-          },
+const findRatings = async (trackName, levelFilter) => {
+  // Base pipeline filters for ratings
+  var queryFilters = [
+    {
+      $group: {
+        _id: "$track",
+        level_average: { $avg: "$level_opinion" },
+        count: { $sum: 1 },
+        lowestRating: {
+          $min: { minRating: "$level_opinion", author: "$author" },
         },
-        { $sort: { level_average: -1 } },
-      ])
-      .toArray();
-  } else {
-    return await tracksDB
-      .collection("ratings")
-      .aggregate([
-        { $match: { track: trackName } },
-        {
-          $group: {
-            _id: "$track",
-            level_average: { $avg: "$level_opinion" },
-            count: { $sum: 1 },
-            lowestRating: {
-              $min: { minRating: "$level_opinion", author: "$author" },
-            },
-            highestRating: {
-              $max: { maxRating: "$level_opinion", author: "$author" },
-            },
-          },
+        highestRating: {
+          $max: { maxRating: "$level_opinion", author: "$author" },
         },
-        { $sort: { level_average: -1 } },
-      ])
-      .toArray();
+      },
+    },
+    { $sort: { level_average: -1 } },
+  ];
+
+  if (levelFilter === undefined) {
+    queryFilters.unshift({ $match: { track: trackName } });
+  } else if (trackName === undefined && levelFilter !== undefined) {
+    queryFilters.unshift({
+      $match: {
+        level_opinion: {
+          $gte: Number(levelFilter),
+          $lt: Number(levelFilter) + 1,
+        },
+      },
+    });
   }
+  return await tracksDB.collection("ratings").aggregate(queryFilters).toArray();
   // Returns an array of track objects
 };
 
