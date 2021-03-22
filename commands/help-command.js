5f04cbb5feda39289c51a43a28893c663ce015c9
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-03-21 23:46:02 -0500

    Add arguments to list command

--- commands/help-command.js
+++ commands/help-command.js
@@ -6,9 +6,9 @@ module.exports = {
       "```ini\n" +
         "commands: --------------[optional-arguments] ------------------ Command Info \n" +
         " - !help \n" +
-        " - !rate                [track-name] [ninja-level]            - Rate a track \n" +
-        " - !list                                                      - list tracks and their ratings\n" +
-        " - !rider               [rider-name] [level-filter]           - List all of a rider's ratings \n" +
+        " - !rate                [track-name], [ninja-level]           - Rate a track \n" +
+        " - !list                [track-name], [ninja-level]           - list tracks and their ratings\n" +
+        " - !rider               [rider-name], [ninja-level]           - List all of a rider's ratings \n" +
         " - !report              [b/f]                                 - Report a bug or feature to the bot \n" +
         " - !delete              [track-name]                          - Delete a rating that you made previously```"
     );
