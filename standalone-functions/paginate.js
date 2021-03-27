32f2fe0fa8605be026edc7490cbddf0b1f85b054
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-03-27 06:43:37 -0500

    Add page header to paginate function

--- standalone-functions/paginate.js
+++ standalone-functions/paginate.js
@@ -1,5 +1,7 @@
-const paginate = (response, regex) => {
-  return response.match(regex);
+const paginate = (response, regex, pageHeader) => {
+  let pages = response.match(regex);
+  pages = pages.map((elem) => pageHeader + elem);
+  return pages;
 };
 
 module.exports.paginate = paginate;
