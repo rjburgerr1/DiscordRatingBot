93787ae4d546677642b6c84d2fc43e07911bde60
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-03-27 06:11:10 -0500

    Add median/mode, reformat toString method

--- standalone-functions/find-ratings.js
+++ standalone-functions/find-ratings.js
@@ -1,4 +1,7 @@
 const { tracksDB } = require("../data/mongodb-utility.js");
+const { getMedian } = require("../standalone-functions/getMedianMongo");
+
+const { getMode } = require("../standalone-functions/getModeMongo");
 const findRatings = async (trackName, levelFilter) => {
   // Base pipeline filters for ratings
   var queryFilters = [
@@ -21,7 +24,6 @@ const findRatings = async (trackName, levelFilter) => {
         track: "$_id",
         count: 1,
         level_average: { $round: ["$level_average", 2] }, // round level_average to hundreths place before projecting
-
         lowestRating: 1,
         highestRating: 1,
       },
@@ -40,10 +42,19 @@ const findRatings = async (trackName, levelFilter) => {
       },
     });
   }
-  return await tracksDB.collection("ratings").aggregate(queryFilters).toArray();
+  medianRatings = await getMedian(tracksDB, "ratings");
+  modeRatings = await getMode(tracksDB, "ratings");
+  allRatings = await tracksDB
+    .collection("ratings")
+    .aggregate(queryFilters)
+    .toArray();
+  let mergedArray = mergeArrays(medianRatings, allRatings);
+  mergedArray = mergeArrays(modeRatings, mergedArray);
+  return mergedArray;
   // Returns an array of track objects
 };
 
+// Method to find specific level ratings i.e.  7.3 or whole range level. i.e. 7-7.99
 const setDecOrInt = (levelFilter) => {
   if (levelFilter.indexOf(".") == -1) {
     return [levelFilter, Number(levelFilter) + 0.99];
@@ -52,4 +63,15 @@ const setDecOrInt = (levelFilter) => {
   }
 };
 
+const mergeArrays = (firstArray, secondArray) => {
+  let merged = [];
+
+  for (let i = 0; i < firstArray.length; i++) {
+    merged.push({
+      ...firstArray[i],
+      ...secondArray.find((itmInner) => itmInner.track === firstArray[i].track),
+    });
+  }
+  return merged;
+};
 exports.findRatings = findRatings;
