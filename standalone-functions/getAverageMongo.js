0deda1cfdbc13fdba8f0f904a954dd7ebb23f995
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-03-28 01:34:07 -0500

    Refactor findRatings function and separate parts to standalone functions and migrate them back to list command component and join result arrays

---
+++ standalone-functions/getAverageMongo.js
@@ -0,0 +1,69 @@
+const getAverage = async (database, collection, trackName, levelFilter) => {
+  // Base pipeline filters for average ratings
+  let queryFilters = [
+    {
+      $group: {
+        _id: "$track",
+        level_average: { $avg: "$level_opinion" },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        track: "$_id",
+        level_average: { $round: ["$level_average", 2] }, // round level_average to hundreths place before projecting
+      },
+    },
+    { $sort: { level_average: -1 } },
+  ];
+  queryFilters = await buildQueryFilters(queryFilters, trackName, levelFilter);
+  const average = await database
+    .collection(collection)
+    .aggregate(queryFilters)
+    .toArray();
+  return average;
+};
+
+const buildQueryFilters = async (queryFilters, trackName, levelFilter) => {
+  if (trackName === undefined && levelFilter !== undefined) {
+    let [lowerBoundLevel, higherBoundLevel] = setDecOrInt(levelFilter);
+    queryFilters.splice(1, 0, {
+      $match: {
+        level_average: {
+          $gte: Number(lowerBoundLevel),
+          $lte: Number(higherBoundLevel),
+        },
+      },
+    });
+  } else if (trackName !== undefined && levelFilter === undefined) {
+    queryFilters.splice(0, 0, {
+      $match: {
+        track: trackName,
+      },
+    });
+  } else if (trackName !== undefined && levelFilter !== undefined) {
+    let [lowerBoundLevel, higherBoundLevel] = setDecOrInt(levelFilter);
+    queryFilters.splice(1, 0, {
+      $match: {
+        track: trackName,
+        level_average: {
+          $gte: Number(lowerBoundLevel),
+          $lte: Number(higherBoundLevel),
+        },
+      },
+    });
+  }
+
+  return queryFilters;
+};
+
+// Method to find specific level ratings i.e.  7.3 or whole range level. i.e. 7-7.99
+const setDecOrInt = (levelFilter) => {
+  if (levelFilter.indexOf(".") == -1) {
+    return [levelFilter, Number(levelFilter) + 0.99];
+  } else {
+    return [levelFilter, levelFilter];
+  }
+};
+
+module.exports.getAverage = getAverage;
