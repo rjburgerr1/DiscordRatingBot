7c805cd2a9cd278bb26ba362564d22c4b66d199c
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-04-03 20:43:13 -0500

    Fix delete command to work with one or all track ratings

--- standalone-functions/delete-rider-rating.js
+++ standalone-functions/delete-rider-rating.js
@@ -1,36 +1,48 @@
 const { tracksDB } = require("../data/mongodb-utility.js");
-const deleteDocument = async (rider, track, allRatings) => {
-  let filter = setFilter(rider, track, allRatings);
-  if (track === undefined) {
-    track = "every track";
-  }
+const deleteDocument = async (rider, ratingsToDelete, allRatings) => {
+  const filter = setFilter(rider, ratingsToDelete, allRatings);
+
   try {
-    const response = await tracksDB.collection("ratings").deleteMany(filter);
+    const response = !allRatings
+      ? await tracksDB
+          .collection("ratings")
+          .deleteOne(filter, { collation: { locale: "en", strength: 1 } })
+      : await tracksDB
+          .collection("ratings")
+          .deleteMany(filter, { collation: { locale: "en", strength: 1 } });
+
+    const deletedTracks = await buildDeletedTracksStr(ratingsToDelete);
+
     if (response.deletedCount > 0) {
-      rider.send("```cs\nDeleted rating for '" + track + "'\n```");
+      rider.send("```cs\nDeleted rating(s) for " + deletedTracks + "\n```");
     } else {
-      rider.send("```cs\nCould not delete rating(s) for'" + track + "'\n```");
+      rider.send(
+        "```cs\nCould not delete rating(s) for " + deletedTracks + "\n```"
+      );
     }
   } catch (error) {
     console.log(error);
   }
 };
 
-const setFilter = (rider, track, allRatings) => {
-  var filter = {
-    author: rider.username,
-    track: track,
-  };
-  filter["collation"] = { locale: "en", strength: 1 };
+const buildDeletedTracksStr = async (ratingsToDelete) => {
+  let tracks = "";
+  ratingsToDelete.forEach((rating) => {
+    tracks += "'" + rating.track + "', ";
+  });
+  return tracks.substr(0, tracks.length - 2);
+};
 
-  if (allRatings) {
-    (filter = {
-      author: rider.username,
-    }),
-      {
-        collation: { locale: "en", strength: 1 },
+const setFilter = (rider, ratingsToDelete, allRatings) => {
+  const filter = !allRatings
+    ? {
+        author: rider.username,
+        track: ratingsToDelete[0].track,
+      }
+    : {
+        author: rider.username,
       };
-  }
+
   return filter;
 };
 
